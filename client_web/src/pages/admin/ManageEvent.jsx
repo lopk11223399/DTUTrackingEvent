a3155a9f25e74edd6e7759822a5a1fbd8a821544
loinguyen45 <0704554312l@gmail.com>
2023-11-20 13:46:27 +0700

    nguyendinhloi

--- client_web/src/pages/admin/ManageEvent.jsx
+++ client_web/src/pages/admin/ManageEvent.jsx
@@ -228,24 +228,19 @@ function ManageEvent({ navigate }) {
 
   return (
     <div className="px-[12px] pt-[70px]">
-      <div className="z-10 flex justify-between items-center bg-white fixed top-0 right-0 left-[22%] px-[12px] shadow-table">
+      <div className="z-10 flex justify-between items-center bg-white fixed top-0 right-0 left-[327px] px-[12px] shadow-table">
         <h1 className="text-[24px] font-[700] text-[#408A7E]">
           Quản lý sự kiện
         </h1>
         <div className="flex gap-[17px] py-[15px]">
-          <div className="w-[292px]  flex items-center gap-3 rounded-tr-[5px] rounded-br-[5px] rounded-tl-[7px] rounded-bl-[7px] border border-[#408A7E]">
-            <label
-              htmlFor="search-event"
-              className="bg-[#408A7E] h-[34px] px-[14px] flex items-center justify-center cursor-pointer rounded-tl-[5px] rounded-bl-[5px]"
-            >
-              <FaSearch color="white" />
-            </label>
+          <div className="w-[292px] px-[12px] py-[6px] flex items-center gap-3 rounded-[4px] border border-[#408A7E]">
+            <span>
+              <AiOutlineSearch size={16} color="#868686" />
+            </span>
             <input
-              id="search-event"
-              autoComplete="off"
               value={searchText}
               onChange={(text) => setSearchText(text.target.value)}
-              placeholder="Tìm kiếm sự kiện"
+              placeholder="Tìm kiếm"
               type="text"
               className="bg-transparent flex-1 outline-none text-[14px] font-[400] text-[#408A7E] placeholder:text-[#868686]"
             />
@@ -262,28 +257,30 @@ function ManageEvent({ navigate }) {
       </div>
       <div className="rounded-[8px]">
         <table className="w-full">
-          <thead className="h-[50px] rounded-t-xl shadow-table">
-            <tr className=" text-[#408A7E] border-b border-[#408A7E]">
-              <td className="w-[5%] text-[14px] font-[700]  text-center py-2">
+          <thead className="h-[68px] rounded-[8px] bg-white shadow-table">
+            <tr className="">
+              <td className="w-[5%] text-[14px] font-[700] text-[#5F5F5F] text-center py-2">
                 #
               </td>
-              <td className="w-[15%] text-[14px] font-[700]  text-center">
+              <td className="w-[15%] text-[14px] font-[700] text-[#5F5F5F] text-center">
                 Ảnh
               </td>
-              <td className="w-[30%] text-[14px] font-[700] ">Tên sự kiện</td>
-              <td className="w-[10%] text-[14px] font-[700]  text-center">
+              <td className="w-[30%] text-[14px] font-[700] text-[#5F5F5F]">
+                Tên sự kiện
+              </td>
+              <td className="w-[10%] text-[14px] font-[700] text-[#5F5F5F] text-center">
                 Loại
               </td>
-              <td className="w-[10%] text-[14px] font-[700]  text-center">
+              <td className="w-[10%] text-[14px] font-[700] text-[#5F5F5F] text-center">
                 Trạng thái
               </td>
-              <td className="w-[10%] text-[14px] font-[700]  text-center">
+              <td className="w-[10%] text-[14px] font-[700] text-[#5F5F5F] text-center">
                 Ngày tạo
               </td>
-              <td className="w-[10%] text-[14px] font-[700]  text-center">
+              <td className="w-[10%] text-[14px] font-[700] text-[#5F5F5F] text-center">
                 Duyệt
               </td>
-              <td className="w-[10%] text-[14px] font-[700]  text-center">
+              <td className="w-[10%] text-[14px] font-[700] text-[#5F5F5F] text-center">
                 Không duyệt
               </td>
             </tr>
