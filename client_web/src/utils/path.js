4b30071d0678d54d6ac746eb2fb789b0fd33aa7c
loinguyen45 <0704554312l@gmail.com>
2023-12-06 21:25:12 +0700

    nguyen dinh loi 06/12

--- client_web/src/utils/path.js
+++ client_web/src/utils/path.js
@@ -1,27 +1,28 @@
 export const pathAdmin = {
-	// Admin
-	ADMIN: 'admin',
-	MANAGE_USERS: 'manage-users',
-	MANAGE_EVENT: 'manage-event',
-	CALENDAR: 'calendar',
-	USERDETAIL: 'userdetail',
-	USERDETAIL_UID: 'userdetail/:uid',
-}
+  // Admin
+  ADMIN: "admin",
+  MANAGE_USERS: "manage-users",
+  MANAGE_EVENT: "manage-event",
+  CALENDAR: "calendar",
+  USERDETAIL: "userdetail",
+  USERDETAIL_UID: "userdetail/:uid",
+};
 
 export const pathCreator = {
-	// Member
-	CREATOR: 'creator',
-	CREATE_EVENT: 'create-event',
-	LIST_EVENT: 'list-event',
-	UPDATE: 'update-event',
-	UPDATE_EID: 'update-event/:eid',
-}
+  // Member
+  CREATOR: "creator",
+  CREATE_EVENT: "create-event",
+  LIST_EVENT: "list-event",
+  UPDATE: "update-event",
+  UPDATE_EID: "update-event/:eid",
+};
 
 export const common = {
-	LOGIN: 'login',
-	DASHBOARD: 'dashboard',
-	PROFILE: 'profile',
-	PROFILE_UID: 'profile/:uid',
-	DETAILEVENT: 'detail-event',
-	DETAILEVENT_EID: 'detail-event/:eid',
-}
+  LOGIN: "login",
+  DASHBOARD: "dashboard",
+  PROFILE: "profile",
+  PROFILE_UID: "profile/:uid",
+  DETAILEVENT: "detail-event",
+  DETAILEVENT_EID: "detail-event/:eid",
+  FORGOTPASSWORD: "forgot",
+};
