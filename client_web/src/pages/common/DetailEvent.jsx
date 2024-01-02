430672be68c896853edf0b91ffe34142599c00cb
loinguyen45 <0704554312l@gmail.com>
2024-01-02 20:44:15 +0700

    nguyendinhloi 02/01/2024

--- client_web/src/pages/common/DetailEvent.jsx
+++ client_web/src/pages/common/DetailEvent.jsx
@@ -78,46 +78,45 @@ function DetailEvent({ navigate, dispatch }) {
           </span>
           <span className="text-[18px] font-[600]">Quay lại</span>
         </div>
-        {current.roleId === 2 && (
-          <div className="flex items-center gap-[6px]">
-            <div
-              onClick={() =>
-                dispatch(
-                  showModal({
-                    isShowModal: true,
-                    modalChildren: (
-                      <CommentModal data={data} eid={eid} render={render} />
-                    ),
-                  })
-                )
-              }
-              className="flex items-center gap-[14px] py-[6px] px-[24px] cursor-pointer border border-[#408A7E] rounded-[4px] bg-[#408A7E] text-white"
-            >
-              <span>
-                <BsChatRightTextFill size={14} />
-              </span>
-              <span className="text-[14px] font-[400]">Bình luận</span>
-            </div>
-            <div
-              onClick={handleDownloadCSV}
-              className="flex items-center gap-[14px] py-[6px] px-[24px] text-[#408A7E] bg-white border border-[#408A7E] cursor-pointer rounded-[4px] hover:bg-[#408A7E] hover:text-white"
-            >
-              <span className="text-[14px] font-[400]">
-                Dach sách người tham gia
-              </span>
-              <span>
-                <TfiImport size={16} />
-              </span>
-              <span className="hidden">
-                <CSVLink
-                  id="csv"
-                  filename={"DanhSachNguoiThamGia.csv"}
-                  data={dataCSV}
-                />
-              </span>
-            </div>
+
+        <div className="flex items-center gap-[6px]">
+          <div
+            onClick={() =>
+              dispatch(
+                showModal({
+                  isShowModal: true,
+                  modalChildren: (
+                    <CommentModal data={data} eid={eid} render={render} />
+                  ),
+                })
+              )
+            }
+            className="flex items-center gap-[14px] py-[6px] px-[24px] cursor-pointer border border-[#408A7E] rounded-[4px] bg-[#408A7E] text-white"
+          >
+            <span>
+              <BsChatRightTextFill size={14} />
+            </span>
+            <span className="text-[14px] font-[400]">Bình luận</span>
           </div>
-        )}
+          <div
+            onClick={handleDownloadCSV}
+            className="flex items-center gap-[14px] py-[6px] px-[24px] text-[#408A7E] bg-white border border-[#408A7E] cursor-pointer rounded-[4px] hover:bg-[#408A7E] hover:text-white"
+          >
+            <span className="text-[14px] font-[400]">
+              Dach sách người tham gia
+            </span>
+            <span>
+              <TfiImport size={16} />
+            </span>
+            <span className="hidden">
+              <CSVLink
+                id="csv"
+                filename={"DanhSachNguoiThamGia.csv"}
+                data={dataCSV}
+              />
+            </span>
+          </div>
+        </div>
       </div>
       <div className="w-full flex items-center">
         <div className="w-[50%] flex items-center gap-[32px]">
